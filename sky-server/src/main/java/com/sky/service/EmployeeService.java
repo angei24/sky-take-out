63fb928e15940f7cc36482d934b83a059301776c
itcast <hello@itcast.cn>
2024-09-18 17:17:59 +0800

    员工分页查询

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -2,17 +2,18 @@ package com.sky.service;
 
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
 
 public interface EmployeeService {
 
-    /**
-     * 员工登录
-     */
+    //员工登录
     Employee login(EmployeeLoginDTO employeeLoginDTO);
 
-    /**
-     *新增员工
-     */
+    //新增员工
     void save(EmployeeDTO employeeDTO);
+
+    //员工分页查询
+    PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
